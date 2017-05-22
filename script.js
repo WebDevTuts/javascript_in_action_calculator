bb54f24dcc00cfe4be39151f8b1bf631bcfa211a
edwinchen85 <edwinchen85@gmail.com>
2017-05-22 20:17:52 +0800

    Declare completed state variable. Check completed state and calc value. Reset to default state.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const buttons = document.getElementsByClassName('cbutton');
 const output = document.getElementById('output');
 let calc = "";
+let completed = false;
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function() {
     let val = this.innerHTML;
+    if (completed || calc=="0") {
+      completed = false;
+      calc = "";
+    }
 
     calc += val;
     output.innerHTML = calc;
