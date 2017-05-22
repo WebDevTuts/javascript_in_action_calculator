35d3a474eb794dc7a6e67daea7de3f0036ac7207
edwinchen85 <edwinchen85@gmail.com>
2017-05-22 20:06:37 +0800

    Declare output variable

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const buttons = document.getElementsByClassName('cbutton');
+const output = document.getElementById('output');
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function() {
