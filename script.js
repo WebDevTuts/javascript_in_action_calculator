1bf1c8343ddc208f1cec04dabb6c30f754a31a45
edwinchen85 <edwinchen85@gmail.com>
2017-05-22 20:12:56 +0800

    Declare calc variable. Join all clicked button and set to output display.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const buttons = document.getElementsByClassName('cbutton');
 const output = document.getElementById('output');
+let calc = "";
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function() {
     let val = this.innerHTML;
-    output.innerHTML = val;
+
+    calc += val;
+    output.innerHTML = calc;
   });
 }
