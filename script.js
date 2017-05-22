3c695053c09d0258c0ce3c5548583dd833c93920
edwinchen85 <edwinchen85@gmail.com>
2017-05-22 20:04:33 +0800

    Add click event listener to each button

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const buttons = document.getElementsByClassName('cbutton');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    console.log('clicked');
+  });
+}
